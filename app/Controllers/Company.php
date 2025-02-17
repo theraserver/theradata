95992060aa14b0b240da3916fb7c1ee596c5a164
Theradata <theraserver@gmail.com>
2025-02-17 10:36:35 +0700

    first commit

--- app/Controllers/Company.php
+++ app/Controllers/Company.php
@@ -15,23 +15,23 @@ class Company extends BaseController
     public function store()
     {
         if ($this->request->is('post')) {
-            $post = $this->request->getPost();
+            // $post = $this->request->getPost();
 
-            $data = [
-                'nama_cabang' => $post['store_name'],
-                'alamat' => $post['store_address'],
-                'telepon' => $post['store_phone_number'],
-                'provinsi' => $post['store_province'],
-                'kota' => $post['store_city'],
-                'kode_pos' => $post['store_post_code']
-            ];
-            if (empty($post['id'])) {
-                $this->storeModel->save($data);
-                $msg = 'Data Cabang Baru berhasil dibuat!';
-            } else {
-                // Update Data
-            }
-            return redirect()->back()->with('success', $msg);
+            // $data = [
+            //     'nama_cabang' => $post['store_name'],
+            //     'alamat' => $post['store_address'],
+            //     'telepon' => $post['store_phone_number'],
+            //     'provinsi' => $post['store_province'],
+            //     'kota' => $post['store_city'],
+            //     'kode_pos' => $post['store_post_code']
+            // ];
+            // if (empty($post['id'])) {
+            //     $this->storeModel->save($data);
+            //     $msg = 'Data Cabang Baru berhasil dibuat!';
+            // } else {
+            //     // Update Data
+            // }
+            // return redirect()->back()->with('success', $msg);
         } else {
             $stores = $this->storeModel->findAll();
             $data = [
